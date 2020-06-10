0a11ac6ba634e58feabf33f0e1f0da62667df622
Tim Gianitsos <timgianitsos@users.noreply.github.com>
2020-06-09 20:55:25 -0700

    Train and test labels

--- orl-database-of-faces/main.py
+++ orl-database-of-faces/main.py
@@ -35,8 +35,10 @@ def display_example():
 	plt.show()
 
 def get_data():
-	train = torch.empty(size=(280, 1, 112, 92), dtype=torch.uint8)
-	test = torch.empty(size=(120, 1, 112, 92), dtype=torch.uint8)
+	train_features = torch.empty(size=(280, 1, 112, 92), dtype=torch.uint8)
+	train_labels = torch.empty(size=(280,), dtype=torch.uint8)
+	test_features = torch.empty(size=(120, 1, 112, 92), dtype=torch.uint8)
+	test_labels = torch.empty(size=(120,), dtype=torch.uint8)
 	i = 0
 	for root, dirs, files in os.walk(dirname(__file__), topdown=True):
 		dirs.sort(key=lambda w: (len(w), w)) #sort by length first, then lexicographically
@@ -44,19 +46,25 @@ def get_data():
 		for filename in files:
 			if filename.endswith(f'{os.extsep}pgm'):
 				if i % 10 < 7:
-					train[(i % 10) + (i // 10) * 7] = read_pgm(open(join(root, filename), 'rb')).unsqueeze(0)
+					train_features[(i % 10) + (i // 10) * 7] = read_pgm(open(join(root, filename), 'rb')).unsqueeze(0)
+					train_labels[(i % 10) + (i // 10) * 7] = (i // 10)
 				else:
-					test[((i - 7) % 10) + (i // 10) * 3] = read_pgm(open(join(root, filename), 'rb')).unsqueeze(0)
+					test_features[((i - 7) % 10) + (i // 10) * 3] = read_pgm(open(join(root, filename), 'rb')).unsqueeze(0)
+					test_labels[((i - 7) % 10) + (i // 10) * 3] = (i // 10)
 				i += 1
-	return train, test
+	return train_features, train_labels, test_features, test_labels
 
 def write_data():
-	train, test = get_data()
-	train_path = join(dirname(__file__), f'train{os.extsep}pt')
-	test_path = join(dirname(__file__), f'test{os.extsep}pt')
-	torch.save(train, train_path)
-	torch.save(test, test_path)
-	print(f'Successfully saved "{train_path}" and "{test_path}"')
+	train_features, train_labels, test_features, test_labels = get_data()
+	train_features_path = join(dirname(__file__), f'train_features{os.extsep}pt')
+	train_labels_path = join(dirname(__file__), f'train_labels{os.extsep}pt')
+	test_features_path = join(dirname(__file__), f'test_features{os.extsep}pt')
+	test_labels_path = join(dirname(__file__), f'test_labels{os.extsep}pt')
+	torch.save(train_features, train_features_path)
+	torch.save(train_labels, train_labels_path)
+	torch.save(test_features, test_features_path)
+	torch.save(test_labels, test_labels_path)
+	print(f'Successfully saved "{train_features_path}", "{train_labels_path}", "{test_features_path}", "{test_labels_path}"')
 
 def train():
 	n = Net()
